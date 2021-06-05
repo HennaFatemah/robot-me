57a6950f40b9d4199e0c60c4f5a0e160cc7ec03c
Henna Fatemah <henna_fatemah@yahoo.com>
2021-06-04 21:01:25 -0400

    searchfield added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React, { Component } from 'react';
 import './App.css';
-import React from 'react';
 import CardsList from './components/CardsList/CardsList';
 import { robots } from './robots'
+import SearchBox from './components/SearchBox/SearchBox';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Robot Me</h1>
-      <CardsList
-        robots={robots}
-      />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (e) => {
+    this.setState({
+      searchField: e.target.value
+    })
+  }
+  render() {
+    const filteredRobots = this.state.robots.filter(robot=>{
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1>Robot Me</h1>
+        <SearchBox
+          searchChange={this.onSearchChange}
+        />
+        <CardsList
+          robots={filteredRobots}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
